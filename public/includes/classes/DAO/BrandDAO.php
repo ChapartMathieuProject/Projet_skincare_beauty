507437ea4085a974172d9cf8c6c347c4cbbd1381
Mathieu Chapart <mathieuchapartdv@gmail.com>
2026-07-09 09:22:24 +0200

    Création de BrandDAO et Brand.php sans raccordement aux fichiers front

---
+++ public/includes/classes/DAO/BrandDAO.php
@@ -0,0 +1,30 @@
+<?php
+
+class BrandDAO extends DAO {
+    protected string $table     = "brands";
+    protected string $primaryKey = "brand_id";
+
+    protected function hydrate(array $row): object {
+        return new Brand($row);
+    }
+
+    #[Override]
+    protected function dehydrate(object $entite): array
+    {
+        return [
+            "brand_name"    => $entite->getName(),
+            "producer_id"   => $entite->getProducerId(),
+        ];
+    }
+
+    public function findAllKeyeById(): array {
+        $stmt = $this->pdo->query("SELECT * FROM ORDER BY brand_name");
+        $brands = [];
+        foreach ($stmt->fetchAll() as $row) {
+            $brands[(int) $row["brand_id"]] = new Brand($row);
+        }
+        return $brands;
+    }
+}
+
+?>
