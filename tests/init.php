088a2eeb7c599daa8c9b14f79c0beda92482bcad
Mathieu Chapart <mathieuchapartdv@gmail.com>
2026-07-28 13:40:20 +0200

    Création des dossiers et fichiers pour préparation tests unitaires + ajout workflow GitHub Actions PHPUnit

---
+++ tests/init.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../public/includes/classes/DAO/DAO.php';
+
+require_once __DIR__ . '/../public/includes/classes/Entity/Product.php';
+require_once __DIR__ . '/../public/includes/classes/Entity/Brand.php';
+require_once __DIR__ . '/../public/includes/classes/Entity/ProductType.php';
+
+require_once __DIR__ . '/../public/includes/classes/DAO/ProductDAO.php';
+require_once __DIR__ . '/../public/includes/classes/DAO/BrandDAO.php';
+require_once __DIR__ . '/../public/includes/classes/DAO/ProductTypeDAO.php';
