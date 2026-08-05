4d21a1b9d2224fa3f580be69419031460f801808
Mathieu Chapart <mathieuchapartdv@gmail.com>
2026-08-05 11:45:03 +0200

    Fix init BDD Docker : import via 00-init.sh (bug DELIMITER entrypoint) + normalisation LF

---
+++ sql/00-init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Contournement du bug entrypoint MySQL (--comments + DELIMITER, cf. docker-library/mysql#746) :
+# on importe le script SQL manuellement, sans l option --comments.
+echo ">> [00-init.sh] Import de creation_database.sql"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --protocol=socket -hlocalhost < /docker-entrypoint-initdb.d/data/creation_database.sql
+echo ">> [00-init.sh] Import termine"
